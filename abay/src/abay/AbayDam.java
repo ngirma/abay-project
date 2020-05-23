93d42ef4cb54efb93c1387ba94d65b13f08bc708
ngirma <girmame83@gmail.com>
2020-05-22 20:01:27 -0700

    Abay dam update

---
+++ abay/src/abay/AbayDam.java
@@ -0,0 +1,10 @@
+package abay;
+
+public class AbayDam {
+
+	public static void main(String[] args) {
+		System.out.println("Congratulation Ethiopia");
+
+	}
+
+}
---
+++ abay/src/abay/Tekeze.java
@@ -0,0 +1,10 @@
+package abay;
+
+public class Tekeze {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
